b5621549388a0c77af4446bf5d95b555605dab2e
hissmirk <false9@kakao.com>
2022-10-05 10:01:36 +0900

    구구단 웹팩 빌드 설정

---
+++ 1.구구단/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+  name: 'gugudan-setting',
+  mode: 'development', // 실서비스: production
+  devtool: 'eval',
+  resolve: {
+    extensions: ['.jsx', '.js'],
+  },
+
+  entry: {
+    app: './client.jsx', //
+  }, // 입력
+
+  module: {
+    rules: [{
+      test: /\.jsx?/,
+      loader: 'babel-loader',
+      options: {
+        presets: [
+          ['@babel/preset-env', {
+            targets: {
+              browsers: ['> 1% in KR'], // browserslist
+            },
+            debug: true,
+          }],
+          '@babel/preset-react',
+        ],
+        plugins: ['@babel/plugin-proposal-class-properties'],
+      },
+    }],
+  },
+
+  output: {
+    filename: 'app.js',
+    path: path.join(__dirname, 'dist'), // __dirname = 현재 폴더, 'dist' = 경로
+  }, // 출력
+};
