9ae3eec1bc3f26ff626020e98a3a2bbbd3b7f823
hissmirk <false9@kakao.com>
2022-10-05 18:37:17 +0900

    함수형 컴포넌트 추가

--- 2.끝말잇기/WordRelay.jsx
+++ 2.끝말잇기/WordRelay.jsx
@@ -1,16 +1,43 @@
 const React = require('react');
-const { Component } = React;
+const { useState, useRef } = require("react");
 
-class WordRelay extends Component {
-  state = {
-    text: 'Hello, webpack.',
+const WordRelay = () => {
+  const [word, setWord] = useState('어드민');
+  const [value, setValue] = useState('');
+  const [result, setResult] = useState('');
+  const inputRef = useRef(null);
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    if (word[word.length - 1] === value[0]) {
+      setWord((prevWord) => {
+        return value;
+      })
+      setResult('⭕');
+      setValue('');
+      inputRef.current.focus();
+    } else {
+      setResult('❌');
+      setValue('');
+      inputRef.current.focus();
+    }
   };
 
-  render() {
-    return (
-      <h1>{this.state.text}</h1>
-    )
+  const onChangeInput = (e) => {
+    setValue(e.currentTarget.value);
   }
+
+  return (
+    <>
+      <div>{word}</div>
+      <form onSubmit={onSubmitForm}>
+        <label htmlFor="wordInput"></label>
+        <input id="wordInput" className="wordInput" ref={inputRef} value={value} onChange={onChangeInput}/>
+        <button>입력</button>
+      </form>
+      <div>{result}</div>
+    </>
+  )
 }
 
 module.exports = WordRelay;
