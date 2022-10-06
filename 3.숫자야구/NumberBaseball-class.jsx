36f6cdb8cef80ebccb34a1886d4dc2c55ff8b01b
hissmirk <false9@kakao.com>
2022-10-06 17:45:22 +0900

    클래스형 컴포넌트 코드

---
+++ 3.숫자야구/NumberBaseball-class.jsx
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+
+import Try from "./Try";
+
+function getNumbers() {
+  const candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const array = [];
+  for (let i = 0; i < 4; i += 1) {
+    const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+    array.push(chosen);
+  }
+  return array;
+}
+
+class NumberBaseball extends Component {
+  state = {
+    result: '',
+    value: '',
+    answer: getNumbers,
+    tries: [],
+  };
+// 9
+  onSubmitForm = (e) => {
+    const { value, answer, tries } = this.state; // 구조분해
+    console.log('tries.length : ' + tries.length);
+    e.preventDefault();
+    if (value === answer.join('')) {
+      this.setState((prevState) => {
+        return {
+          result: 'Home run! ' + answer.join('') + ' ⚾',
+          tries: [...tries, { try: value, result: 'Home run!' }],
+        }
+      });
+      alert('Restart.');
+      this.setState({
+        value: '',
+        answer: getNumbers(),
+        tries: [],
+      });
+    } else {
+      const answerArray = value.split('').map((v) => parseInt(v));
+      let strike = 0;
+      let ball = 0;
+      if (tries.length >= 9) {
+        this.setState({
+          result: `Failed! ${answer.join('')}`,
+        });
+        alert('Restart.');
+        this.setState({
+          value: '',
+          answer: getNumbers(),
+          tries: [],
+        });
+      } else {
+        for (let i = 0; i < 4; i += 1) {
+          if (answerArray[i] === answer[i]) {
+            strike += 1;
+          } else if (answer.includes(answerArray[i])) {
+            ball += 1;
+          }
+          this.setState({
+            tries: [...tries, { try: value, result: `${strike} Strike, ${ball} Ball` }],
+            value: '',
+          })
+        }
+      }
+    }
+  };
+
+  onChangeInput = (e) => {
+    this.setState({
+      value: e.currentTarget.value,
+    });
+  };
+
+  render() {
+    const { result, value, tries } = this.state; // 구조분해
+    return (
+      <>
+        <h1>{result}</h1>
+        <form onSubmit={this.onSubmitForm}>
+          <input maxLength={4} value={value} onChange={this.onChangeInput}/>
+        </form>
+        <div>시도: {tries.length}</div>
+        <ul>
+          {tries.map((v, i) => {
+            return (
+              <Try key={`${i + 1}차 시도 :`} tryInfo={v} index={i}/>
+            );
+          })}
+        </ul>
+      </>
+    )
+  }
+}
+
+export default NumberBaseball;
--- 3.숫자야구/client.jsx
+++ 3.숫자야구/client.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import NumberBaseball from './NumberBaseball';
+import NumberBaseball from './NumberBaseball-hooks';
 
 ReactDOM.createRoot(document.querySelector('#root')).render(<NumberBaseball />);
