c94a3eaaf6970d9f6540523a5567b43a831bb0c3
hissmirk <false9@kakao.com>
2022-10-14 18:38:18 +0900

    로또 추첨기 추가, 다양한 라이프 사이클 활용

---
+++ 6.로또/Lotto.jsx
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+import Ball from "./Ball";
+
+function getWinNumbers() {
+  console.log('getWinNumbers');
+  const candidate = Array(45).fill().map((v, i) => i + 1);
+  const shuffle = [];
+  while (candidate.length > 0) {
+    shuffle.push(candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]);
+  }
+  const bonusNumber = shuffle[shuffle.length - 1];
+  const winNumbers = shuffle.splice(0, 6).sort((p, c) => p - c);
+  return [...winNumbers, bonusNumber];
+}
+
+class Lotto extends Component {
+  state = {
+    winNumbers: getWinNumbers(),
+    winBalls: [],
+    bonus: null,
+    redo: false,
+  }
+
+  timeouts = [];
+
+  runTimeouts = () => {
+    console.log('runTimeouts');
+    const { winNumbers } = this.state;
+    for (let i = 0; i < winNumbers.length - 1; i++) {
+      this.timeouts[i] = setTimeout(() => {
+        this.setState((prevState) => {
+          return {
+            winBalls: [...prevState.winBalls, winNumbers[i]],
+          }
+        })
+      }, (i + 1) * 1000);
+    }
+    this.timeouts[6] = setTimeout(() => {
+      this.setState({
+        bonus: winNumbers[6],
+        redo: true,
+      });
+    }, 7000);
+  }
+
+  componentDidMount() {
+    console.log('didMount');
+    this.runTimeouts();
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    const { winBalls, bonus, redo } = this.state;
+
+    if (winBalls.length === 0 && bonus === null && redo === false) {
+      console.log('didUpdate => true');
+      this.runTimeouts();
+    } else {
+      console.log('didUpdate => false');
+    }
+  }
+
+  componentWillUnmount() {
+    this.timeouts.forEach((v) => {
+      clearTimeout(v);
+    });
+  }
+
+  onClickRedo = () => {
+    console.log('onClickRedo');
+    this.setState({
+      winNumbers: getWinNumbers(),
+      winBalls: [],
+      bonus: null,
+      redo: false,
+    });
+    this.timeouts = [];
+  };
+
+  render() {
+    const { winBalls, bonus, redo } = this.state;
+    return (
+      <>
+        <div>LOTTO RESULT</div>
+        <div>
+          {winBalls.map((v) => <Ball key={v} number={v}/>)}
+        </div>
+        <div>BONUS!</div>
+        {bonus && <Ball number={bonus}/>}
+        {redo && <button onClick={this.onClickRedo}>한 번 더</button>}
+      </>
+    );
+  }
+}
+
+export default Lotto;
