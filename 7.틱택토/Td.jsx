a4f0ab638c601240f49f87cad17fa8f46a61dfe1
현주엽/소프트브리지/화상커뮤니케이션플랫폼 <false9@kakao.com>
2022-11-08 17:41:26 +0900

    구현하기

--- 7.틱택토/Td.jsx
+++ 7.틱택토/Td.jsx
@@ -4,9 +4,11 @@ import { CLICK_CELL, CHANGE_TURN } from "./TicTacToe";
 const Td = ({ rowIndex, cellIndex, dispatch, cellData }) => {
   const onClickTd = useCallback(() => {
     console.log(rowIndex, cellIndex);
+    if (cellData) {
+      return;
+    }
     dispatch({ type: CLICK_CELL, row: rowIndex, cell: cellIndex });
-    dispatch({ type: CHANGE_TURN });
-  }, []);
+  }, [cellData]);
 
   return (
       <td onClick={onClickTd}>{cellData}</td>
