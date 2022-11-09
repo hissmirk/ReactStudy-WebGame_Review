b9f60e3e9152d15e27c224cb295b768f85de8823
현주엽/소프트브리지/화상커뮤니케이션플랫폼 <false9@kakao.com>
2022-11-09 18:30:19 +0900

    지뢰찾기 게임 구성 설정

---
+++ 8.지뢰찾기/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+module.exports = {
+  name: 'mine-search-setting',
+  mode: 'development',
+  devtool: 'eval',
+  resolve: {
+    extensions: ['.jsx', '.js'],
+  },
+
+  entry: {
+    app: './client.jsx'
+  },
+
+  module: {
+    rules: [{
+      test: /\.jsx?/,
+      loader: 'babel-loader',
+      options: {
+        presets: [
+          ['@babel/preset-env', {
+            targets: {
+              browsers: ['> 1% in KR'],
+            },
+            debug: true,
+          }],
+          '@babel/preset-react',
+        ],
+      },
+    }],
+  },
+
+  plugins: [
+    new RefreshWebpackPlugin(),
+  ],
+
+  output: {
+    filename: 'app.js',
+    path: path.join(__dirname, 'dist'),
+  },
+
+  devServer: {
+    devMiddleware: { publicPath: '/dist' },
+    static: { directory: path.resolve(__dirname, 'src') },
+    hot: true,
+  },
+
+};
