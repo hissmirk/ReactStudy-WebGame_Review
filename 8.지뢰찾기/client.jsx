d8f3ce0f45dc51195fee24604a97638eb9835c72
현주엽/소프트브리지/화상커뮤니케이션플랫폼 <false9@kakao.com>
2022-11-10 18:22:55 +0900

    파일 이름 수정

--- 8.지뢰찾기/client.jsx
+++ 8.지뢰찾기/client.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import MineSearch from './mineSearch';
+import MineSearch from './MineSearch';
 
 ReactDOM.createRoot(document.querySelector('#root')).render(<MineSearch />);
